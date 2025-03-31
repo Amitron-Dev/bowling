7270ffae26ef457c2e40879b569f1c6340ed1850
Amitron-Dev <martingrellier79@gmail.com>
2025-03-31 08:13:35 +0200

    init

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+input.onButtonPressed(Button.B, function () {
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 70)
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 70)
+    basic.pause(1000)
+    Kitronik_Move_Motor.stop()
+})
+basic.forever(function () {
+	
+})
